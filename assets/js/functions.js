9680879574f890e00b6a6bcc1f6bf56198b16ecd
KieronDowie <kierondowie@gmail.com>
2016-11-18 21:15:30 +0200

    Fixed the header problems using fittext

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$('.spanText').focus(highlight);
 	$('a').click(scrollToLink)
+    jQuery(".hero h1").fitText(1, { minFontSize: '100px', maxFontSize: '400px' });
 });
 
 function scrollToLink(e){
@@ -44,3 +45,36 @@ jQuery.fn.selectText = function(){
         selection.addRange(range);
     }
 };
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
